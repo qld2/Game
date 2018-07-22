481ad40c46c24eceb11e37c57cbc2b2f3bdc5713
Quinn Donovan <qld2@illinois.edu>
2018-07-22 13:06:14 -0500

    Added roundcontroller

---
+++ Roundcontroller.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "ofMain.h"
+#include "Entity_headers\Enemy.h"
+#include "Entity_headers\Player.h"
+
+class Roundcontroller {
+	int enemiesRemaining;
+	int MAX_ENEMIES = 100;
+
+	Stopwatch c1;
+	ofVec3f a, b, c;
+
+	Player& player;
+	vector<Enemy> enemies;
+
+public:
+	Roundcontroller(Player& player);
+	~Roundcontroller();
+
+	void update();
+	void updateRound();
+	void draw() const;
+
+private:
+	void spawnEnemy();
+
+};
